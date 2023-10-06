1c15d26e4df2bc85146c28c6e72857fa75881f3e
gerzytet <gerzytet@gmail.com>
2023-10-06 19:59:48 -0400

    Frog move.  IN BOTH DIRECTIONS!

--- Assets/Frog.cs
+++ Assets/Frog.cs
@@ -17,28 +17,34 @@ public class Frog : MonoBehaviour
     [SerializeField] private float rotationRate;
 
     [SerializeField] private GameObject frontFoot;
+    [SerializeField] private GameObject backFoot;
     [SerializeField] private GameObject handReferencePoint;
     private Vector3 targetOffset;
     
     [SerializeField] private GameObject frontHand;
     private FrogState state = FrogState.IDLE;
+    private int count = 0;
     public float fixLegsRate;
 
     private bool flippedLeft = false;
+    public float timeScale;
     IEnumerator Jump()
     {
         state = FrogState.JUMPING;
-        while (frontFoot.GetComponent<ToggleCollider>().IsColliding())
+        float iterationCount = 0;
+        while (frontFoot.GetComponent<ToggleCollider>().IsColliding() || iterationCount < 5)
         {
-            Debug.Log(frontFoot.transform.position.y);
+            iterationCount++;
             body.GetComponent<Rigidbody2D>().AddForce(jumpDirection);
             body.transform.Rotate(0, 0, rotationRate);
-            yield return null;
+            Debug.Log("Jumping " + count);
+            yield return new WaitForFixedUpdate();
         }
 
         while (!frontHand.GetComponent<ToggleCollider>().IsColliding())
         {
-            yield return null;
+            Debug.Log("waiting to land: " + count);
+            yield return new WaitForFixedUpdate();
         }
         
         state = FrogState.LANDING;
@@ -48,11 +54,12 @@ public class Frog : MonoBehaviour
     void Start()
     {
         targetOffset = handReferencePoint.transform.position - frontFootTarget.transform.position;
-        StartCoroutine(Jump());
+        //StartCoroutine(Jump());
     }
 
     private void AdjustTargets()
     {
+        Debug.Log("Adjusting targets " + count);
         Vector3 targetTarget = handReferencePoint.transform.position - targetOffset;
         Vector3 currentKnee = frontFoot.transform.position;
         Vector3 pos = Vector3.MoveTowards(currentKnee, targetTarget, fixLegsRate);
@@ -68,33 +75,52 @@ public class Frog : MonoBehaviour
     {
         if (this.flippedLeft != flippedLeft)
         {
+            body.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            body.GetComponent<Rigidbody2D>().angularVelocity = 0;
             //rotationRate = -rotationRate;
             targetOffset.x = -targetOffset.x;
             jumpDirection.x = -jumpDirection.x;
-            body.transform.localScale = new Vector3(-body.transform.localScale.x, body.transform.localScale.y, body.transform.localScale.z);
+            body.transform.rotation = Quaternion.Euler(body.transform.rotation.eulerAngles.x, flippedLeft ? 180 : 0, body.transform.rotation.eulerAngles.z);
+            float frontFootTargetOffset = body.transform.position.x - frontFootTarget.transform.position.x;
+            frontFootTarget.transform.position = new Vector3(body.transform.position.x + frontFootTargetOffset, frontFootTarget.transform.position.y, frontFootTarget.transform.position.z);
+            float backFootTargetOffset = body.transform.position.x - backFootTarget.transform.position.x;
+            backFootTarget.transform.position = new Vector3(body.transform.position.x + backFootTargetOffset, backFootTarget.transform.position.y, backFootTarget.transform.position.z);
+            
+            frontFoot.transform.localScale = new Vector3(-frontFoot.transform.localScale.x, frontFoot.transform.localScale.y, frontFoot.transform.localScale.z);
+            backFoot.transform.localScale = new Vector3(-backFoot.transform.localScale.x, backFoot.transform.localScale.y, backFoot.transform.localScale.z);
             this.flippedLeft = flippedLeft;
         }
     }
 
     private void FixedUpdate()
     {
+        count++;
+        if (count < 50)
+        {
+            return;
+        }
         if (state == FrogState.LANDING)
         {
             AdjustTargets();
         }
         
-        if (Input.GetKey(KeyCode.D))
-        {
-            SetFlip(false);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            SetFlip(true);
-        }
+        
 
-        if (state == FrogState.IDLE && (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)))
+        if (state == FrogState.IDLE) //&& (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)))
         {
+            if (Input.GetKey(KeyCode.D))
+            {
+                SetFlip(false);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                SetFlip(true);
+            }
+            state = FrogState.JUMPING;
+            Debug.Log("Starting jump coroutine " + count);
             StartCoroutine(Jump());
         }
+
+        Time.timeScale = this.timeScale;
     }
 }
