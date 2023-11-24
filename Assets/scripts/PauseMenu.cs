6ad790ce2d5b4f73480da10aec831f40a98428f7
Patrick-Mayer <53142589+Patrick-Mayer@users.noreply.github.com>
2023-11-24 17:23:10 -0500

    Added pause menu

---
+++ Assets/scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject PauseMenu_UI;
+
+    public bool pauseButtonPressed()
+    {
+        return (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7));
+    }
+
+    public void enterPauseMenu(){
+        PauseMenu_UI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void resumeGame(){
+        PauseMenu_UI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void returnToMainMenu(){
+        //Debug.Log("going to main menu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pauseButtonPressed()){
+            if (gameIsPaused){
+                resumeGame();
+            }else{
+                enterPauseMenu();
+            }
+        }
+    }
+}
